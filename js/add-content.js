94a631d4aed403770686dc08c5d9ace464c35924
Candice Bianca Brown <candicebrown97@gmail.com>
2017-11-24 04:34:10 +0000

    added completed practice files for c01

---
+++ js/add-content.js
@@ -0,0 +1,12 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+    greeting = 'Good Evening!';
+} else if (hourNow > 12) {
+    greeting = 'Good Afternoon!';
+} else {
+    greeting = 'Welcome!';
+};
+document.write('<h3' + greeting + '</h3>');
